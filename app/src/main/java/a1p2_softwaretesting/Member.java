6a2d96e6ab91eb8326887caa9a3a32209feaac39
bryanMawan <140645318+bryanMawan@users.noreply.github.com>
2024-12-08 16:49:11 +0000

    GREEN: Passed test for Member init

---
+++ app/src/main/java/a1p2_softwaretesting/Member.java
@@ -0,0 +1,15 @@
+package a1p2_softwaretesting;
+
+public class Member {
+    private String name;
+    private String id;
+
+    public Member(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() { return name; }
+    public String getId() { return id; }
+}
+
